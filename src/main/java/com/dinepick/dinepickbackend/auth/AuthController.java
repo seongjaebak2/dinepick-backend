a020a32178219f8951c948c66296e2b7e31b55cf
KDH <rkdehdgns200@naver.com>
2025-12-30 15:53:07 +0900

    feat: add auth

---
+++ src/main/java/com/dinepick/dinepickbackend/auth/AuthController.java
@@ -0,0 +1,42 @@
+package com.dinepick.dinepickbackend.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(
+            @RequestBody Map<String, String> req
+    ) {
+        authService.signup(
+                req.get("email"),
+                req.get("password")
+        );
+        return ResponseEntity.ok("회원가입 성공");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+            @RequestBody Map<String, String> req
+    ) {
+        String token = authService.login(
+                req.get("email"),
+                req.get("password")
+        );
+        return ResponseEntity.ok(
+                Map.of("accessToken", token)
+        );
+    }
+}

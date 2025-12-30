5057281c2ebb4b4cf46f703031b65ffbc8e300ab
SeongJaeBak <uuuu8882979@gmail.com>
2025-12-30 09:11:35 +0900

    Initial commit: create project

---
+++ src/main/java/com/dinepick/dinepickbackend/DinepickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dinepick.dinepickbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinepickBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinepickBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dinepick/dinepickbackend/DinepickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinepick.dinepickbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinepickBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

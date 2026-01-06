409fda415c31bbb2cbbce5a09c923ba3bf68b2d6
SeongJaeBak <uuuu8882979@gmail.com>
2026-01-06 16:29:27 +0900

    feat: add category filter

---
+++ src/main/java/com/dinepick/dinepickbackend/entity/Category.java
@@ -0,0 +1,10 @@
+package com.dinepick.dinepickbackend.entity;
+
+public enum Category {
+    KOREAN,
+    JAPANESE,
+    CHINESE,
+    WESTERN,
+    CAFE,
+    ETC
+}
--- src/main/java/com/dinepick/dinepickbackend/service/RestaurantService.java
+++ src/main/java/com/dinepick/dinepickbackend/service/RestaurantService.java
@@ -1,6 +1,7 @@
 package com.dinepick.dinepickbackend.service;
 
 import com.dinepick.dinepickbackend.dto.RestaurantResponse;
+import com.dinepick.dinepickbackend.entity.Category;
 import com.dinepick.dinepickbackend.entity.Restaurant;
 import com.dinepick.dinepickbackend.exception.RestaurantNotFoundException;
 import com.dinepick.dinepickbackend.repository.RestaurantRepository;
@@ -16,17 +17,22 @@ import java.util.List;
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
 
-    //    레스토랑 목록 + 검색 + 페이징
+    //    레스토랑 목록 + 검색 + 페이징 + 카테고리 검색
     public Page<RestaurantResponse> findRestaurants(
             String keyword,
+            Category category,
             Pageable pageable
     ) {
         Page<Restaurant> page;
 
-        if (keyword == null || keyword.isBlank()) {
+        if ((keyword == null || keyword.isBlank()) && category == null) {
             page = restaurantRepository.findAll(pageable);
-        } else {
+        } else if (keyword != null && category == null) {
             page = restaurantRepository.findByNameContaining(keyword, pageable);
+        } else if (keyword == null) {
+            page = restaurantRepository.findByCategory(category, pageable);
+        } else {
+            page = restaurantRepository.findByNameContainingAndCategory(keyword, category, pageable);
         }
         return page.map(RestaurantResponse::from);
     }

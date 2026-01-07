9e104e8319dcb3e6c00cd234f69b55f555a8b5b5
KDH <rkdehdgns200@naver.com>
2026-01-07 16:01:34 +0900

    docs : access token 유효시간 수정

--- src/main/java/com/dinepick/dinepickbackend/security/JwtTokenProvider.java
+++ src/main/java/com/dinepick/dinepickbackend/security/JwtTokenProvider.java
@@ -27,7 +27,7 @@ public class JwtTokenProvider {
      * 토큰 만료 시간 (1시간)
      * 1000ms * 60초 * 30분
      */
-    private static final long ACCESS_EXPIRE = 1000 * 60; // 30분
+    private static final long ACCESS_EXPIRE = 1000 * 60 * 30; // 30분
     private static final long REFRESH_EXPIRE = 1000L * 60 * 60 * 24 * 7; // 7일
 
     public JwtTokenProvider() {
